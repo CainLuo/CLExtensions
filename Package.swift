1540527bc5c6e2e696b4b6a2cf1145fbf44eb55a
CainLuo <350116542@qq.com>
2019-09-27 12:21:37 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,16 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-let package = Package(
-    name: "CLExtensions",
-    platforms: [
-        .iOS(.v10)
-    ],
-    products: [
-        .library(
-            name: "CLExtensions",
-            targets: ["CLExtensions"]),
-    ],
-    targets: [
-        .target(
-            name: "CLExtensions",
-            path: "CLExtensions"),
-    ],
-    swiftLanguageVersions: [.v5]
-)
+let package = Package(name: "CLExtensions",
+                      platforms: [.macOS(.v10_12),
+                                  .iOS(.v10),
+                                  .tvOS(.v10),
+                                  .watchOS(.v3)],
+                      products: [.library(name: "CLExtensions",
+                                          targets: ["CLExtensions"])],
+                      targets: [.target(name: "CLExtensions",
+                                        path: "Source")],
+                      swiftLanguageVersions: [.v5])
+
