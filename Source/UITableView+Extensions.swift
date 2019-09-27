120f80a63dd4269cde0f151cc7dfc6654285c456
CainLuo <350116542@qq.com>
2019-09-27 12:21:03 +0800

    Create UITableView+Extensions.swift

---
+++ Source/UITableView+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  UITableView+Extensions.swift
+//  CLExtensions
+//
+//  Created by CainLuo on 2019/9/14.
+//  Copyright © 2019年 CainLuo. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    public func register(nibName: String, inBundle bundle: Bundle? = nil) {
+        self.register(UINib(nibName: nibName, bundle: bundle),
+                      forCellReuseIdentifier: nibName)
+    }
+    
+    public func registerHeaderFooterView(nibName: String, inBundle bundle: Bundle? = nil) {
+        self.register(UINib(nibName: nibName, bundle: bundle),
+                      forHeaderFooterViewReuseIdentifier: nibName)
+    }
+    
+    public func hideEmptyCells() {
+        tableFooterView = UIView(frame: .zero)
+    }
+}
