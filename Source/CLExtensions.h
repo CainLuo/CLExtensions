b658ffa10addfc88a4ab9b48db48df52a36d6da2
CainLuo <350116542@qq.com>
2019-09-27 12:21:33 +0800

    Create CLExtensions.h

---
+++ Source/CLExtensions.h
@@ -0,0 +1,19 @@
+//
+//  CLExtensions.h
+//  CLExtensions
+//
+//  Created by Cain Luo on 2019/9/27.
+//  Copyright © 2019 Cain Luo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CLExtensions.
+FOUNDATION_EXPORT double CLExtensionsVersionNumber;
+
+//! Project version string for CLExtensions.
+FOUNDATION_EXPORT const unsigned char CLExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CLExtensions/PublicHeader.h>
+
+
