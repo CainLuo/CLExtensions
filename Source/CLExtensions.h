d3d6b4ed4c049884a16a6b1053b6c4c6500fa7d8
CainLuo <350116542@qq.com>
2019-09-27 12:32:22 +0800

    Update CLExtensions.h

--- Source/CLExtensions.h
+++ Source/CLExtensions.h
@@ -8,12 +8,5 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for CLExtensions.
 FOUNDATION_EXPORT double CLExtensionsVersionNumber;
-
-//! Project version string for CLExtensions.
 FOUNDATION_EXPORT const unsigned char CLExtensionsVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <CLExtensions/PublicHeader.h>
-
-
