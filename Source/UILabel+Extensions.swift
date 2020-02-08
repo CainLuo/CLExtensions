b2a50a33843b5fde580286d67655bcf141473b59
cainluo <350116542@qq.com>
2020-02-08 20:03:15 +0800

    Update UILabel+Extensions.swift

--- Source/UILabel+Extensions.swift
+++ Source/UILabel+Extensions.swift
@@ -10,11 +10,14 @@ import UIKit
 
 @IBDesignable open class IBDesignableLabel: UILabel {
     
-    @IBInspectable var tipsImage: UIImage = UIImage()
+    @IBInspectable var tipsImage: UIImage?
     @IBInspectable var isPrefix: Bool = true
+    @IBInspectable var hightLightText: String?
+    @IBInspectable var hightLightColor: UIColor = .black
 
     override open func draw(_ rect: CGRect) {
-        if let text = text {
+        if let tipsImage = tipsImage,
+            let text = text {
             let iconRect = CGRect(x: 0,
                                   y: UIScreen.fitPlusScreen(value: -8),
                                   width: UIScreen.fitPlusScreen(value: 50),
@@ -26,7 +29,23 @@ import UIKit
                                                                                    isPrefix: isPrefix)
         }
         
+        if let hightLightText = hightLightText {
+            drawLabelHightLightText(hightLightText)
+        }
+        
         super.draw(rect)
+    }
+    
+    public func drawLabelHightLightText(_ hightLightText: String) {
+        if let text = text {
+            let contentString: NSString = text as NSString
+            let mutableAttributedString = NSMutableAttributedString(string: contentString as String)
+            
+            mutableAttributedString.addAttributes([.foregroundColor: hightLightColor],
+                                                  range: contentString.range(of: hightLightText))
+            
+            attributedText = mutableAttributedString
+        }
     }
 }
 
