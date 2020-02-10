c746133018e52a02ea7e1984e0b6528b59325e1a
cainluo <350116542@qq.com>
2020-02-10 22:05:26 +0800

    Create ErrorTracker.swift

---
+++ CLExtensions+Rx/ErrorTracker.swift
@@ -0,0 +1,47 @@
+//
+//  ErrorTracker.swift
+//  LockTools
+//
+//  Created by Cain Luo on 2019/11/14.
+//  Copyright © 2019 Cain Luo. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+public class ErrorTracker: SharedSequenceConvertibleType {
+    public typealias SharingStrategy = DriverSharingStrategy
+    
+    private let _subject = PublishSubject<Error>()
+    
+    init() {
+        
+    }
+    
+    public func trackerError<O: ObservableConvertibleType>(from source: O) -> Observable<O.Element> {
+        return source.asObservable().do(onError: onError)
+    }
+    
+    public func asSharedSequence() -> SharedSequence<SharingStrategy, Error> {
+        return _subject.asObservable().asDriverOnErrorJustComplete()
+    }
+    
+    public func asObservable() -> Observable<Error> {
+        return _subject.asObserver()
+    }
+    
+    private func onError(_ error: Error) {
+        _subject.onNext(error)
+    }
+    
+    deinit {
+        _subject.onCompleted()
+    }
+}
+
+extension ObservableConvertibleType {
+    public func trackError(_ errorTracker: ErrorTracker) -> Observable<Element> {
+        return errorTracker.trackerError(from: self)
+    }
+}
