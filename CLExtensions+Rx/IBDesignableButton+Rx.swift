53a83dffbf148be621133bb3467711909fba9858
cainluo <350116542@qq.com>
2020-02-10 22:05:24 +0800

    Create IBDesignableButton+Rx.swift

---
+++ CLExtensions+Rx/IBDesignableButton+Rx.swift
@@ -0,0 +1,20 @@
+//
+//  IBDesignableButton.swift
+//  CLExtensions
+//
+//  Created by Cain Luo on 2020/2/10.
+//  Copyright © 2020 Cain Luo. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+import CLExtensions
+
+//extension Reactive where Base: IBDesignableButton {
+//    public var canSubmit: Binder<Bool> {
+//        return Binder(self.base) { button, canSubmit in
+//            button.canSubmit = canSubmit
+//        }
+//    }
+//}
