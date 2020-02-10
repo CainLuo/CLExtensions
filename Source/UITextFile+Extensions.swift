3b1df8b5335c0d12bf0fbd48849df1e603eb8dcc
cainluo <350116542@qq.com>
2020-02-10 22:04:30 +0800

    Update UITextFile+Extensions.swift

--- Source/UITextFile+Extensions.swift
+++ Source/UITextFile+Extensions.swift
@@ -11,20 +11,6 @@ import UIKit
 private var maxLengths = [UITextField: Int]()
 private var minLengths = [UITextField: Int]()
 
-@IBDesignable open class IBDesignableTextField: UITextField {
-    @IBInspectable var placeholderColor: UIColor = .clear
-
-    open override func drawPlaceholder(in rect: CGRect) {
-        let placeholserAttributes = [NSAttributedString.Key.foregroundColor : placeholderColor,
-                                     NSAttributedString.Key.font : self.font]
-        
-        self.attributedPlaceholder = NSAttributedString(string: self.placeholder ?? "",
-                                                        attributes: placeholserAttributes as [NSAttributedString.Key : Any])
-        
-        super.drawPlaceholder(in: rect)
-    }
-}
-
 extension UITextField {
     public func checkLength(max: Int) {
         guard let text = text else {
