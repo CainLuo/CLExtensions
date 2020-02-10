5222ef69362e42b44606039930c41bb0254c3253
cainluo <350116542@qq.com>
2020-02-10 22:05:12 +0800

    Create CLExtensions.h

---
+++ CLExtensions/CLExtensions.h
@@ -0,0 +1,12 @@
+//
+//  CLExtensions.h
+//  CLExtensions
+//
+//  Created by Cain Luo on 2019/9/27.
+//  Copyright © 2019 Cain Luo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double CLExtensionsVersionNumber;
+FOUNDATION_EXPORT const unsigned char CLExtensionsVersionString[];
