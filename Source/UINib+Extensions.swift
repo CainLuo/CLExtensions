05965e85e54ae924106ca4ce1712bd4581011c15
cainluo <350116542@qq.com>
2020-02-10 22:04:34 +0800

    Update UINib+Extensions.swift

--- Source/UINib+Extensions.swift
+++ Source/UINib+Extensions.swift
@@ -20,3 +20,42 @@ extension UINib {
         return UINib(nibName: nibName, bundle: bundle).instantiate(withOwner: nil, options: nil)[0] as! UIView
     }
 }
+
+// MARK: UIView
+extension UIView {
+    public func view(fromNib nib: String) -> UIView? {
+        return UINib(nibName: nib, bundle: nil).instantiate(withOwner: self, options: nil).first
+            as? UIView
+    }
+    
+    public static func fromNib(nib: String) -> UIView? {
+        guard let view = UINib(nibName: nib, bundle: nil)
+            .instantiate(withOwner: self, options: nil)
+            .first as? UIView else {
+                assertionFailure("Nib not found")
+                return nil
+        }
+
+        return view
+    }
+}
+
+// MARK: CustomNib
+public protocol CustomNib {
+    associatedtype CustomNibType
+    
+    static func fromNib(nib: String) -> CustomNibType?
+}
+
+extension CustomNib {
+    public static func fromNib(nib: String) -> Self? {
+        guard let view = UINib(nibName: nib, bundle: nil)
+            .instantiate(withOwner: self, options: nil)
+            .first as? Self else {
+                assertionFailure("Nib not found")
+                return nil
+        }
+
+        return view
+    }
+}
