aff85a50bf6c6582d6849d02cd9d3d64a24b9233
cainluo <350116542@qq.com>
2020-04-09 22:51:48 +0800

    Update Array+Extensions.swift

--- Source/Array+Extensions.swift
+++ Source/Array+Extensions.swift
@@ -42,6 +42,17 @@ extension Array where Element: Equatable {
         let filteredList = newElements.filter({!self.contains($0)})
         self.append(contentsOf: filteredList)
     }
+
+    public mutating func filterDuplicates<E: Equatable>(_ filter: (Element) -> E) -> [Element] {
+        var result = [Element]()
+        for value in self {
+            let key = filter(value)
+            if !result.map({filter($0)}).contains(key) {
+                result.append(value)
+            }
+        }
+        return result
+    }
 }
 
 extension Array {
