436fd08de9e7eaea3c7eab602c99b182fd9d4322
CainLuo <350116542@qq.com>
2020-04-15 23:21:55 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,11 +3,21 @@
 
 import PackageDescription
 
-let package = Package(name: "CLExtensions",
-                      platforms: [.iOS(.v10)],
-                      products: [.library(name: "CLExtensions",
-                                          targets: ["CLExtensions"])],
-                      targets: [.target(name: "CLExtensions",
-                                        path: "Source")],
-                      swiftLanguageVersions: [.v5])
+let package = Package(
+    name: "CLExtensions",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "CLExtensions", targets: ["CLExtensions"]),
+        .library(name: "CLExtensions+Rx", targets: ["CLExtensions+Rx"]),
+        .library(name: "CLExtensions+Log", targets: ["CLExtensions+Log"]),
+        .library(name: "CLExtensions+Localize", targets: ["CLExtensions+Localize"])
+    ],
+    targets: [
+        .target(name: "CLExtensions", path: "Source")
+        .target(name: "CLExtensions+Rx", path: "CLExtensions+Rx")
+        .target(name: "CLExtensions+Log", path: "CLExtensions+Log")
+        .target(name: "CLExtensions+Localize", path: "CLExtensions+Localize")
+    ],
+    swiftLanguageVersions: [.v5]
+)
 
